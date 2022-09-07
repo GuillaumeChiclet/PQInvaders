d5bef75c199d9754ed0d52ac0ad7eedbb60d0c3d
Guillaume Chiclet <g.chiclet@student.isartdigital.com>
2022-09-07 11:59:54 +0200

    fix broken git

--- PQ2D/Assets/Scripts/Rooms/RoomManager.cs
+++ PQ2D/Assets/Scripts/Rooms/RoomManager.cs
@@ -16,5 +16,11 @@ public class RoomManager : MonoBehaviour
         }
     }
 
-
+    public void LaunchWave(int day, int period, float duration)
+    {
+        for (int i = 0; i < enemySpawners.Length; i++)
+        {
+            enemySpawners[i].LaunchWave(day, period, duration);
+        }
+    }
 }
