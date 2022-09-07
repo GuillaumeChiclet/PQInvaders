4b1970636b866f157eb615a38fd7131485d52dc0
Guillaume Chiclet <g.chiclet@student.isartdigital.com>
2022-09-07 11:22:33 +0200

    add player melee

--- PQ2D/Assets/Scripts/Player/PlayerMelee.cs
+++ PQ2D/Assets/Scripts/Player/PlayerMelee.cs
@@ -4,18 +4,53 @@ using UnityEngine;
 
 public class PlayerMelee : MonoBehaviour
 {
-
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float range = 3.0f;
+    [SerializeField] private float coneAngle = 30.0f;
+    [SerializeField] private float timeBetween2Attacks = 0.5f;
+    private float timer = 0.0f;
 
     private void Update()
     {
+        if (timer > 0.0f)
+        {
+            timer -= Time.deltaTime;
+            return;
+        }
+
         if (Input.GetButtonDown("Fire2"))
         {
             Attack();
+            timer = timeBetween2Attacks;
         }
     }
 
     private void Attack()
     {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, range, transform.right, range);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            Vector3 dirToHit = (hit.point - (Vector2)transform.position);
+            float angle = Vector3.Angle(transform.right, dirToHit);
+            if (angle <= coneAngle)
+            {
+                if (hit.collider.gameObject.TryGetComponent<IDamageable>(out IDamageable damageable))
+                {
+                    damageable.Damage(damage);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 left = Quaternion.Euler(0, 0, coneAngle) * transform.right;
+        Vector3 right = Quaternion.Euler(0, 0, -coneAngle) * transform.right;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + left * range);
+        Gizmos.DrawLine(transform.position, transform.position + right * range);
 
     }
 }
